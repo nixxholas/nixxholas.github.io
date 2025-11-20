e5ff9abfd6523a40e1e0e639a4ae5726c6d4a87d
nixxholas <nixholas@icloud.com>
2025-11-21 02:16:59 +0800

    fix: build
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
 module.exports = {
-  extends: [
-    // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
-    'plugin:vue/essential'
-  ],
-
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-
-  root: true,
-
-  env: {
-    node: true,
-  },
-
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  extends: ['next/core-web-vitals'],
 };
